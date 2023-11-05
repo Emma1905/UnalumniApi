2122987296d9b695c9514aeb3879f79859f1f4de
Emmanuel <54013972+Emma192@users.noreply.github.com>
2023-11-04 23:03:57 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+   });
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
